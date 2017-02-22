e40bd95743b1c7f59e485cd99b2691e132124b03
Brad Beltowski <bbelowski@fullsail.com>
2017-02-22 18:40:46 -0500

    Working on getting Chia in the mix

--- test/user-api.spec.js
+++ test/user-api.spec.js
@@ -1,12 +1,15 @@
 require('co-mocha');
+var expect = require('chai').expect;
+var data = require('../test_data/user_api');
 
 describe('user data', function() {
 	it('it should return +1 to the users', function* () {
+		// console.log(`DATA1: ${users}`);
 		var users = yield data.users.get();
+		// console.log(`DATA2: ${users}`);
 		yield data.users.save({name:'Bob'})
 		var newUsers = yield data.users.get();
-
+		// console.log(`DATA3: ${users.length}`);
 		expect('newUsers').to.have.lengthOf(users.length+1);
-
-	})
-} )
+	});
+});
---
+++ test_data/user_api.js
@@ -0,0 +1,8 @@
+module.exports = {
+	users: {
+		get: function* () { 
+			return [];
+		},
+		save: function* () { }
+	}
+};
