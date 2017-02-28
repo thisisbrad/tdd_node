f46082aadc24056af3c622a5d6e5030266b203d4
Brad Beltowski <bbelowski@fullsail.com>
2017-02-27 20:44:18 -0500

    Got the test to pass chai assertion

--- test/user-api.spec.js
+++ test/user-api.spec.js
@@ -1,15 +1,17 @@
 require('co-mocha');
-var expect = require('chai').expect;
-var data = require('../test_data/user_api');
+const expect = require('chai').expect;
+const data = require('../test_data/user_api');
+const fs = require('co-fs');
+
+before(function *(){
+	yield fs.writeFile('./test_data/user.json', '[]')
+})
 
 describe('user data', function() {
 	it('it should return +1 to the users', function* () {
-		// console.log(`DATA1: ${users}`);
-		var users = yield data.users.get();
-		// console.log(`DATA2: ${users}`);
+		const users = yield data.users.get();
 		yield data.users.save({name:'Bob'})
-		var newUsers = yield data.users.get();
-		// console.log(`DATA3: ${users.length}`);
-		expect('newUsers').to.have.lengthOf(users.length+1);
+		const newUsers = yield data.users.get();
+		expect(newUsers).to.have.lengthOf(users.length+1);
 	});
 });
--- test_data/user_api.js
+++ test_data/user_api.js
@@ -1,8 +1,16 @@
+const fs = require('co-fs');
+const userFile = './test_data/user.json';
+
 module.exports = {
 	users: {
-		get: function* () { 
-			return [];
+		get: function* () {
+			const data = yield fs.readFile(userFile, 'utf-8');
+			return JSON.parse(data);
 		},
-		save: function* () { }
+		save: function* (user) { 
+			const users = yield this.get();
+			users.push(user);
+			yield fs.writeFile(userFile, JSON.stringify(users));
+		}
 	}
 };
